b75ea29a98827bfee14b08c4909155e50180dca2
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-05-28 21:39:06 +0900

    회원 기능 구현

---
+++ src/main/java/com/sparta/myselectshop/security/UserDetailsImpl.java
@@ -0,0 +1,36 @@
+package com.sparta.myselectshop.security;
+
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.entity.UserRoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public record UserDetailsImpl(User user) implements UserDetails {
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+}
